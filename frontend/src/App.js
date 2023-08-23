f2431eb7d8997fc2e8d7a8ac147a0807533bf1be
kinyichukwu <kinyichukwuose@gmail.com>
2023-08-23 18:34:08 +0100

    ....

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import "aos/dist/aos.css";
 import { Home } from "./pages/Home";
 import { Nav } from "./components/Nav";
 import Signup from "./pages/Signup";
