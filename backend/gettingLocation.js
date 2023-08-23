163ad503319c5cf0accc964bdc6937c63599977e
Julius170 <juliusayoola5@gmail.com>
2023-08-23 02:47:45 +0100

    getting the locations

---
+++ backend/gettingLocation.js
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Live Location Example</title>
+  <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places"></script>
+</head>
+<body>
+  <p>Driver's Live Location: <span id="driver-location"></span></p>
+  <p>Rider's Live Location: <span id="rider-location"></span></p>
+
+  <script>
+    // Replace with your Google Maps API key
+    const API_KEY = 'YOUR_API_KEY';
+
+    // Variables to store the live locations
+    let driverLiveLocation = null;
+    let riderLiveLocation = null;
+
+    // Function to update the driver's live location
+    function updateDriverLocation() {
+      if (navigator.geolocation) {
+        navigator.geolocation.watchPosition(
+          position => {
+            driverLiveLocation = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+            document.getElementById('driver-location').textContent = `${driverLiveLocation.lat}, ${driverLiveLocation.lng}`;
+          },
+          error => {
+            console.error('Error getting driver location:', error);
+          }
+        );
+      } else {
+        console.error('Geolocation is not supported by this browser.');
+      }
+    }
+
+    // Function to update the rider's live location
+    function updateRiderLocation() {
+      if (navigator.geolocation) {
+        navigator.geolocation.watchPosition(
+          position => {
+            riderLiveLocation = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+            document.getElementById('rider-location').textContent = `${riderLiveLocation.lat}, ${riderLiveLocation.lng}`;
+          },
+          error => {
+            console.error('Error getting rider location:', error);
+          }
+        );
+      } else {
+        console.error('Geolocation is not supported by this browser.');
+      }
+    }
+
+    // Call the updateLocation functions to start tracking
+    updateDriverLocation();
+    updateRiderLocation();
+  </script>
+</body>
+</html>
